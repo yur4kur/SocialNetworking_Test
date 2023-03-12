ddad368ae655862d35470e6db29eecf019f02048
Iurii Kurinnoi <yur4kur@icloud.com>
2023-03-12 14:04:28 +1000

    Initial Commit

---
+++ SocialNetworking_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialNetworking_Test
+//
+//  Created by Юрий Куринной on 12.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
